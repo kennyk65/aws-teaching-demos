be5283164b2b7769c9bd3b2fc60e2704dce153de
Ken Krueger <kenkrueger65@gmail.com>
2016-11-11 12:19:28 -0500

    s

--- ami-lookup-o-matic/AmiLookupOMatic.template.js
+++ ami-lookup-o-matic/AmiLookupOMatic.template.js
@@ -7,7 +7,25 @@
 
   "Mappings" : {  },
 
-  "Resources" : { },
+  "Resources" : { 
+
+    "LambdaFunction": {
+      "Type" : "AWS::Lambda::Function",
+      "Properties" : {
+        "Code" : {
+          "S3Bucket": "kk-uploads", 
+          "S3Key": "AmiLookupOMatic.js.zip"
+        },
+        "Description" : "sample",
+        "FunctionName" :  "jubjub",
+        "Handler" : "index.handler",
+        "MemorySize" : 128,
+        "Role" : "arn:aws:iam::011673140073:role/service-role/AmiLookupOMaticLambdaRole",
+        "Runtime" : "nodejs4.3",
+        "Timeout" : 15
+    }}
+
+  },
 
   "Outputs" : {  }
 }
