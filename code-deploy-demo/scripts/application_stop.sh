845a87392b40a56ed794b7ea9498209507e1f3e5
Ken Krueger <kenkrueger65@gmail.com>
2018-09-20 11:27:04 -0400

    Adjustments to scripts, tweaks to CF template

--- code-deploy-demo/scripts/application_stop.sh
+++ code-deploy-demo/scripts/application_stop.sh
@@ -1,2 +1,5 @@
-# stop it:
-service demoapp stop
+# Do nothing.  Better to put stop activities in "before_install".
+# This script always lags behind the rest of the scripts anyway; 
+# the revision (containing this script) is not downloaded until AFTER this point, 
+# so the script that runs is always from the PREVIOUS revision (ugh).  
+# Since that is needlessly irritating, it is just simpler to do everything in "before_install".
