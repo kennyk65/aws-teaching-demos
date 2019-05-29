46021f2634acfc522b7a77162d1555392794d20e
KennyK <kenkrueger65@gmail.com>
2019-05-29 17:54:49 -0400

    Removed wacky end-of-line characters from windows

--- code-deploy-demo/scripts/application_stop.sh
+++ code-deploy-demo/scripts/application_stop.sh
@@ -3,3 +3,4 @@
 # the revision (containing this script) is not downloaded until AFTER this point, 
 # so the script that runs is always from the PREVIOUS revision (ugh).  
 # Since that is needlessly irritating, it is just simpler to do everything in "before_install".
+exit 0
