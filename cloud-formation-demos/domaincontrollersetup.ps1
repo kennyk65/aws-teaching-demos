e7a964a62476072fd41b23ad7d19185b48dbf6ce
kennyk65 <kenkrueger65@gmail.com>
2021-07-18 02:24:34 -0400

    Update domaincontrollersetup.ps1

--- cloud-formation-demos/domaincontrollersetup.ps1
+++ cloud-formation-demos/domaincontrollersetup.ps1
@@ -1,5 +1,10 @@
 $Password = "RANDOMPASSHERE" | ConvertTo-SecureString -AsPlainText -Force
 
+
 New-ADUser -Name "alice" -AccountPassword $Password -SamAccountName alice -DisplayName "alice" -EmailAddress alice@example.com -Enabled $TRUE -GivenName alice -PassThru -PasswordNeverExpires $TRUE -UserPrincipalName alice
+
+
 New-ADUser -Name "bob" -AccountPassword $Password -SamAccountName bob -DisplayName "bob" -EmailAddress bob@example.com -Enabled $TRUE -GivenName bob -PassThru -PasswordNeverExpires $TRUE -UserPrincipalName bob
+
+
 New-ADUser -Name "adfssvc" -AccountPassword $Password -SamAccountName adfssvc -DisplayName "adfssvc" -EmailAddress adfssvc@example.com -Enabled $TRUE -GivenName adfssvc -PassThru -PasswordNeverExpires $TRUE -UserPrincipalName adfssvc
