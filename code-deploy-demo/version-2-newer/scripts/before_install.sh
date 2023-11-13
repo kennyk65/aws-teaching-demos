7f57f43b2fd66d99f61667c3ef1450843ded03ad
Ken Krueger <kenkrueger65@gmail.com>
2023-11-12 20:27:00 -0500

    java 17, boot 3

--- code-deploy-demo/version-2-newer/scripts/before_install.sh
+++ code-deploy-demo/version-2-newer/scripts/before_install.sh
@@ -1,13 +1,10 @@
 # Update dependencies:
 yum update -y
-yum install wget -y
-yum install java-1.8.0 -y
-# Make sure a /opt/demo folder exists: 
-mkdir /opt/app
-cd /opt/app
+yum install wget java-17-amazon-corretto -y
+# Make sure a /var/app folder exists: 
+mkdir /var/app
+cd /var/app
 # stop the existing service, if it is running:
-service demoapp stop
-# Remove the symlink so the start script can add it:
-rm -f /etc/init.d/demoapp
+systemctl stop spring-boot-app
 # Delete the old to make way for the new:
 rm -f app.jar
